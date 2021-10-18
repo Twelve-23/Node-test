721930cad5ea02ed9cd81b3e831d0156599f513c
jaysuchak1 <jay@phpdots.com>
2021-10-18 15:23:04 +0530

    summary issue solved

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -11,21 +13,25 @@ app.get('/', (req, res) => {
 app.get('/getData', (req, res) => {
   const month = Number(req.query.month);
   const summary = req.query.summary;
-  if(!month){
+  if (!month) {
     res.send('Missing Month Parameter');
     return;
   }
   let out = [];
-  for(let period of data.periods){
+  for (let period of data.periods) {
     const startMonth = Number(period.period.start.split('-')[1]);
     const endMonth = Number(period.period.end.split('-')[1]);
-    if(startMonth !== month){
+    if (startMonth !== month) {
       continue;
     }
-    if(endMonth !== month){
+    if (endMonth !== month) {
       continue;
     }
-    out.push(period.itemized);
+    if (summary && (summary == "true" || summary == true)) {
+      out.push(period.summary)
+    } else {
+      out.push(period.itemized);
+    }
   }
   res.json(out);
 })
