4531d22165a28abe1c6eb31e41bd5f824701e64d
jaysuchak1 <jay@phpdots.com>
2021-10-18 16:36:34 +0530

    update with dynamic key val store

--- app.js
+++ app.js
@@ -4,13 +4,10 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-// cashing package
+// caching package
 const CacheApi = require('node-filesystem-cache');
 const cachePath = path.join(__dirname, 'cache');
 const Cache = new CacheApi(cachePath);
-
-// lodash lib
-const _ = require('lodash');
 
 const data = require('./data.json');
 
@@ -26,46 +23,42 @@ app.get('/getData', (req, res) => {
     return;
   }
 
-  // check if has on cash
-  if (Cache.has('req')) {
-    const requestCash = Cache.get('req');
-    const responseCash = Cache.get('res');
-    // compare req objects with cash
-    if (_.isEqual(req.query, requestCash)) {
-      console.log("from cash");
-      // return from Cash
-      return res.send(Cache.get('res'));
-    } else {
-      // store req
-      Cache.put('req', req.query);
-    }
+  let keyName = `${month}`
+  if (summary && (summary == "true" || summary == true)) {
+    keyName = `${keyName}-summary`
   } else {
-    // store req
-    Cache.put('req', req.query);
+    keyName = `${keyName}-periods`
   }
-  console.log("from json");
-  let out = [];
-  for (let period of data.periods) {
-    const startMonth = Number(period.period.start.split('-')[1]);
-    const endMonth = Number(period.period.end.split('-')[1]);
-    if (startMonth !== month) {
-      continue;
+
+  // check if has on cash
+  if (Cache.has(keyName)) {
+    res.json(Cache.get(keyName));
+    console.log("from cache", keyName);
+  } else {
+    let out = [];
+    for (let period of data.periods) {
+      const startMonth = Number(period.period.start.split('-')[1]);
+      const endMonth = Number(period.period.end.split('-')[1]);
+      if (startMonth !== month) {
+        continue;
+      }
+      if (endMonth !== month) {
+        continue;
+      }
+      // if summary is given
+      if (summary && (summary == "true" || summary == true)) {
+        out.push(period.summary);
+        // store in cash
+        Cache.put(keyName, out);
+      } else {
+        out.push(period.itemized);
+        // store in cash
+        Cache.put(keyName, out);
+      }
     }
-    if (endMonth !== month) {
-      continue;
-    }
-    // if summary is given
-    if (summary && (summary == "true" || summary == true)) {
-      out.push(period.summary);
-      // store in cash
-      Cache.put('res', out);
-    } else {
-      out.push(period.itemized);
-      // store in cash
-      Cache.put('res', out);
-    }
+    res.json(out);
+    console.log("from json");
   }
-  res.json(out);
 })
 
 app.listen(port, () => {
