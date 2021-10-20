26ad181ef0260e06401fe0ac1fa89b9a1c7eed83
kgondaliya <kajalgondaliya1889@gmail.com>
2021-10-20 18:45:47 +0530

    Implement the 'summary' option in the getData endpoint. Implement a caching system for the getData endpoint.

--- app.js
+++ app.js
@@ -1,35 +1,52 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
+const data = require("./data.json");
+const NodeCache = require("node-cache");
+//checkperiod : expiring the cache after N seconds
+const myCache = new NodeCache({ checkperiod: 60 });
 
-const data = require('./data.json');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
-
-app.get('/getData', (req, res) => {
+app.get("/getData", (req, res) => {
   const month = Number(req.query.month);
-  const summary = req.query.summary;
-  if(!month){
-    res.send('Missing Month Parameter');
+  const summary = Boolean(req?.query?.summary);
+  if (!month) {
+    res.send("Missing Month Parameter");
     return;
   }
-  let out = [];
-  for(let period of data.periods){
-    const startMonth = Number(period.period.start.split('-')[1]);
-    const endMonth = Number(period.period.end.split('-')[1]);
-    if(startMonth !== month){
-      continue;
+
+  if (month) {
+    //create unique cache key for itemized  & summary data save into cache
+    const cacheKey=`month_${month}_summary_${summary}`
+    if (myCache.has(cacheKey)) {
+      return res.json(myCache.get(cacheKey));
+    } else {
+      let out = [];
+      for (let period of data.periods) {
+        const startMonth = Number(period.period.start.split("-")[1]);
+        const endMonth = Number(period.period.end.split("-")[1]);
+        if (startMonth !== month) {
+          continue;
+        }
+        if (endMonth !== month) {
+          continue;
+        }
+        //If the request contains summary=true then instead of sending the period.itemized data back it should send the period.summary data.
+        if (summary) {
+          out.push(period.summary);
+        } else {
+          out.push(period.itemized);
+        }
+      }
+      myCache.set(cacheKey,out);
+      return res.json(out);
     }
-    if(endMonth !== month){
-      continue;
-    }
-    out.push(period.itemized);
   }
-  res.json(out);
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-})
+});
