536e9299f516582fde8292a6039998fdfc9cf2e6
Kaushal <kaushal91198@gmail.com>
2021-11-26 12:19:01 +0530

    wrong

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const NodeCache = require("node-cache");
+const myCache = new NodeCache({ checkperiod: 60 });
 
 const data = require('./data.json');
 
@@ -11,10 +13,28 @@ app.get('/', (req, res) => {
 app.get('/getData', (req, res) => {
   const month = Number(req.query.month);
   const summary = req.query.summary;
+
+  //Generating key for first time request
+  let Key  = `month_${month}`
+  if (summary === "true") {
+  console.log('used summary true condition')
+  Key = `month_${month}_summary`;
+  }
+
   if(!month){
     res.send('Missing Month Parameter');
     return;
   }
+
+  console.log(myCache.keys())
+  //When we get value from cache 
+  let value = myCache.get(Key)
+  if (value) {
+    console.log('cache function is used')
+    const data = myCache.get(Key)
+    return res.json(data);
+  }
+
   let out = [];
   for(let period of data.periods){
     const startMonth = Number(period.period.start.split('-')[1]);
@@ -25,8 +45,9 @@ app.get('/getData', (req, res) => {
     if(endMonth !== month){
       continue;
     }
-    out.push(period.itemized);
+    out.push(summary === 'true' ? period.summary : period.itemized);
   }
+  myCache.set(Key,out);
   res.json(out);
 })
 
